a6b6670da04b0d10843cab14934e35c6eab377bf
Michał Cichecki <mcichecki@me.com>
2020-05-09 21:59:49 +0200

    Update answer view styling

--- emoji-rebus.playground/Sources/Views/Answer/AnswerView.swift
+++ emoji-rebus.playground/Sources/Views/Answer/AnswerView.swift
@@ -4,7 +4,11 @@ protocol AnswerViewDelegate: AnyObject {
     func didTapClose()
 }
 
-final class AnswerView: NSVisualEffectView {
+final class AnswerTextField: NSTextField {
+    override var allowsVibrancy: Bool { true }
+}
+
+final class AnswerView: NSView {
     weak var delegate: AnswerViewDelegate?
     
     var answer: Answer! {
@@ -13,13 +17,13 @@ final class AnswerView: NSVisualEffectView {
         }
     }
     
-    lazy var titleTextField: NSTextField = configure { textField in
+    lazy var titleTextField: AnswerTextField = configure { textField in
         textFieldConfig(textField)
         textField.font = NSFont.systemFont(ofSize: 24.0)
     }
     
     // TODO: Bold word of title
-    lazy var descriptionTextField: NSTextField = configure { textField in
+    lazy var descriptionTextField: AnswerTextField = configure { textField in
         textFieldConfig(textField)
         textField.font = NSFont.systemFont(ofSize: 16.0)
     }
@@ -28,10 +32,16 @@ final class AnswerView: NSVisualEffectView {
         textField.alignment = .center
         textField.isEditable = false
         textField.isBezeled = false
-        textField.textColor = ColorStyle.white
+        textField.textColor = ColorStyle.black
+        textField.backgroundColor = ColorStyle.white
+        textField.wantsLayer = true
+        textField.layer?.cornerRadius = 5.0
+        //        textField.allowsVibrancy = false
         //        textField.backgroundColor = ColorStyle.green
         //        textField.backgroundColor = .yellow
     }
+    
+    private lazy var visualEffectBackgroundView = NSVisualEffectView()
     
     private let closeButton: NSButton = configure { closeButton in
         closeButton.title = "✕"
@@ -49,7 +59,9 @@ final class AnswerView: NSVisualEffectView {
     required init?(coder: NSCoder) { fatalError() }
     
     private func addSubviews() {
-        [titleTextField, descriptionTextField, closeButton].forEach(addSubview(_:))
+        //        [titleTextField, descriptionTextField, closeButton].forEach(addSubview(_:))
+        addSubviews(visualEffectBackgroundView)
+        visualEffectBackgroundView.addSubviews(titleTextField, descriptionTextField, closeButton)
     }
     
     private func setUpConstraints() {
@@ -73,22 +85,30 @@ final class AnswerView: NSVisualEffectView {
              $0.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10.0),
              $0.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -5.0)]
         }
+        
+        visualEffectBackgroundView.activateConstraints {
+            [$0.topAnchor.constraint(equalTo: topAnchor),
+             $0.leadingAnchor.constraint(equalTo: leadingAnchor),
+             $0.trailingAnchor.constraint(equalTo: trailingAnchor),
+             $0.bottomAnchor.constraint(equalTo: bottomAnchor)]
+        }
     }
     
     private func setUpViews() {
-        wantsLayer = true
-//        layer?.backgroundColor = ColorStyle.green.cgColor
+        //        wantsLayer = true
+        //        layer?.backgroundColor = ColorStyle.green.cgColor
         layer?.cornerRadius = 5.0
-//        layer?.borderColor = ColorStyle.lightGreen.cgColor
-//        layer?.borderWidth = 3.0
+        //        layer?.borderColor = ColorStyle.lightGreen.cgColor
+        //        layer?.borderWidth = 3.0
+        //        setBackgroundColor(.red)
         titleTextField.stringValue = "Answer"
         descriptionTextField.stringValue = "Some longer, multiline description..."
         
         closeButton.target = self
         closeButton.action = #selector(closeButtonTapped)
         
-        material = .popover
-        blendingMode = .withinWindow
+        visualEffectBackgroundView.material = .sidebar
+        visualEffectBackgroundView.blendingMode = .withinWindow
     }
     
     @objc private func closeButtonTapped() {
